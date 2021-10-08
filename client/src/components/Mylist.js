6f08f423141f858d3adc9ac233d6ee0ab1fc8b3b
songha <songha@songhaui-MacBookPro.local>
2021-10-09 00:08:17 +0900

    Feat : Add mypage userinfo update

--- client/src/components/Mylist.js
+++ client/src/components/Mylist.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Mylist() {
+  const [recipeList, setRecipeList] = useState("");
+  const [hasLists, setHasLists] = useState(false);
+
+  const hasListsHandler = () => {
+    setHasLists(true);
+  };
   return (
     <div>
       <div className="mylist-wrap">
         <ul>
           <li>
-            <a href="#1">여기에 글 목록이 나옵니다.</a>
+            <a href="#1">- 여기에 글 목록이 나옵니다.</a>
           </li>
           <li>
             <a href="#2">여기에 글 목록이 나옵니다.</a>
--- client/src/pages/Mypage.js
+++ client/src/pages/Mypage.js
@@ -2,8 +2,30 @@ import React, { useState } from "react";
 import Mylist from "../components/Mylist";
 
 function Mypage(props) {
-  const [hasLists, setHasLists] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [clickToEdit, setClickToEdit] = useState(true);
+  const [userinfo, setuserinfo] = useState({
+    password: "",
+    nickname: "",
+  });
+
+  const handleUpdate = () => {
+    // TODO: 유저 정보를 서버에 업데이트 요청하고, 성공한 경우 history.push("/")
+  };
+
+  const handleInputValue = (key) => (e) => {
+    setuserinfo({ ...userinfo, [key]: e.target.value });
+  };
+
+  console.log("userinfo ??? ", userinfo);
+
+  const clickToEditHandler = () => {
+    setClickToEdit(!clickToEdit);
+  };
+  const showModalHandler = () => {
+    setShowModal(!showModal);
+  };
 
   const openHandler = () => {
     setIsOpen(!isOpen);
@@ -25,14 +47,36 @@ function Mypage(props) {
             </div>
             <hr></hr>
             <span className="myinfo">비밀번호</span>
-            <span className="myinfo-props">프랍스에서 받아온 정보</span>
+            {clickToEdit ? (
+              <span className="myinfo-props">프랍스에서 받아온 정보</span>
+            ) : (
+              <input
+                type="password"
+                className=""
+                placeholder="프랍스"
+                onChange={handleInputValue("password")}
+              ></input>
+            )}
             <hr></hr>
             <span className="myinfo">닉네임</span>
-            <span className="myinfo-props">프랍스에서 받아온 정보</span>
+            {clickToEdit ? (
+              <span className="myinfo-props">프랍스에서 받아온 정보</span>
+            ) : (
+              <input
+                type="text"
+                className=""
+                placeholder="props"
+                onChange={handleInputValue("nickname")}
+              ></input>
+            )}
             <hr></hr>
             <div className="btn-wrap">
               <div>
-                <button type="button" className="my-btn" onClick={openHandler}>
+                <button
+                  type="button"
+                  className={clickToEdit ? "my-btn" : "list"}
+                  onClick={openHandler}
+                >
                   내가 쓴 글 목록
                   <span id="plus">
                     {isOpen ? (
@@ -48,23 +92,35 @@ function Mypage(props) {
                   </span>
                 </button>
               </div>
-              <div>
-                <button
-                  type="button"
-                  className="my-btn"
-                  onClick={() => console.log("인풋창으로 바껴야함")}
-                >
-                  개인정보 수정
-                </button>
-              </div>
-              <div>
-                <p
-                  className="getout"
-                  onClick={() => console.log("모달팝업해야함")}
-                >
-                  회원탈퇴
-                </p>
-              </div>
+              {clickToEdit ? (
+                <div>
+                  <button
+                    type="button"
+                    className="my-btn"
+                    onClick={clickToEditHandler}
+                  >
+                    <p>개인정보 수정</p>
+                  </button>
+                </div>
+              ) : (
+                <div>
+                  <button
+                    type="submit"
+                    className="my-btn"
+                    onClick={handleUpdate}
+                  >
+                    <p>수정 완료</p>
+                  </button>
+                </div>
+              )}
+              {clickToEdit ? (
+                <div>
+                  <p className="getout" onClick={showModalHandler}>
+                    회원탈퇴
+                  </p>
+                  {showModal ? "모달창이 뜬다!" : null}
+                </div>
+              ) : null}
             </div>
           </div>
         </div>
