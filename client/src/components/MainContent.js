acb920d66a72db8aba93e7a255c21538d9d7ff0a
songha <songha@songhaui-MacBookPro.local>
2021-10-12 21:30:08 +0900

    TT

--- client/src/components/MainContent.js
+++ client/src/components/MainContent.js
@@ -1,14 +1,29 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
-function MainContent() {
+function MainContent({ recipe }) {
   return (
+    // <div className="itemCard">
+    //   <img src="https://t1.daumcdn.net/cfile/blog/1440AD344D06487323" alt="목업용 이미지"/>
+    //   <h3 className="itemCardTitle">{recipe.recipe_title}</h3>
+    //   <div>{recipe.recipe_subtitle}</div>
+    //   <div>{recipe.recipe_photo}</div>
+    //   <div>{recipe.recipe_content}</div>
+    // </div>
+
     <div className="itemCard">
-      <img src="https://t1.daumcdn.net/cfile/blog/1440AD344D06487323" alt="목업용 이미지"/>
-      <h3 className="itemCardTitle">붕어붕어 빙수</h3>
-      여름에는 시원한 붕어팥빙수!
+      <div>
+        <img src={recipe.recipe_photo} alt={recipe.idx} />
+      </div>
+      <div>
+        <h3 className="itemCardTitle">{recipe.recipe_title}</h3>
+        <div>{recipe.recipe_subtitle}</div>
+      </div>
+      <div>
+        <div>-----------------</div>
+      </div>
     </div>
-  )
-};
+  );
+}
 
 export default MainContent;
