7b19cb7916cb463144c7603e6b3656bb954504a2
이상헌 <bleach8625@gmail.com>
2021-10-13 15:20:04 +0900

    feat/recipeClick

--- client/src/components/MainContent.js
+++ client/src/components/MainContent.js
@@ -1,25 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
-function MainContent({ recipe }) {
+function MainContent({ recipe, handleClickNumRecipe }) {
+  const num = recipe.id
   return (
-    // <div className="itemCard">
-    //   <img src="https://t1.daumcdn.net/cfile/blog/1440AD344D06487323" alt="목업용 이미지"/>
-    //   <h3 className="itemCardTitle">{recipe.recipe_title}</h3>
-    //   <div>{recipe.recipe_subtitle}</div>
-    //   <div>{recipe.recipe_photo}</div>
-    //   <div>{recipe.recipe_content}</div>
-    // </div>
-
-    <div className="itemCard">
-      <div>
-        <img src={`https://muggerbar.ml/${recipe.recipe_photo}`} alt={recipe.idx} />
+    <Link to="./recipe/recipe.id">
+      <div className="itemCard" >
+        <div onClick={()=>(handleClickNumRecipe(num))}>
+          <img src={`https://muggerbar.ml/${recipe.recipe_photo}`} alt={recipe.idx} />
+        </div>
+        <div>
+          <h3 className="itemCardTitle">{recipe.recipe_title}</h3>
+          <div>{recipe.recipe_subtitle}</div>
+        </div>
       </div>
-      <div>
-        <h3 className="itemCardTitle">{recipe.recipe_title}</h3>
-        <div>{recipe.recipe_subtitle}</div>
-      </div>
-    </div>
+    </Link>
   );
 }
 
